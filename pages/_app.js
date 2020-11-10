6ce51ce0e3b6cb597326345924a7c10064fea74c
Taylor Phillips <taylorphillips133@gmail.com>
2020-11-10 14:21:07 -0800

    make progress by getting a valid api call

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,50 @@
-import '../styles/globals.css'
+import React from 'react';
+import { loadStripe }  from '@stripe/stripe-js';
+import { 
+  Elements, 
+  CardElement, 
+  useStripe, 
+  useElements
+} from '@stripe/react-stripe-js';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const {error, paymentMethod} = await stripe.createPaymentMethod({
+      type: 'card',
+      card: elements.getElement(CardElement)
+    });
+
+    if (!error) {
+      console.log(paymentMethod);
+    }
+  }
+
+  return (
+  <form onSubmit={handleSubmit} style={{maxWidth: "400px", margin: "0 auto"}}>
+    <h2>Become a subscriber for $9.99/month</h2>
+    <CardElement />
+    <button type="submit" disable={!stripe}> 
+      Pay
+    </button>
+  </form>
+  );
+};
+
+const stripePromise = loadStripe('pk_test_51HkDM6Hnepz07q3sOlwTDA5r0ConQc1yFJ3XemTKKFKdzILL2Dl2JR5Bm6vmn6YPLlg4n4vCkjeqQdIflhp06uJt00Y9dSLj25');
+// This is a public Key ^^
+
+const Index = () => {
+  return (
+  <Elements stripe={stripePromise}>
+    <CheckoutForm />
+  </Elements>
+  );
+};
+
+export default Index;
